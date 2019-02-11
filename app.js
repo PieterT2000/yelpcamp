e07d37b07e9489f9196c4e097e36033788dc94bb
Pieter <pietervantuijl2000@gmail.com>
2019-02-11 15:38:19 +0000

    add mongolab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ let     commentRoutes   = require("./routes/comments"),
         campgroundRoutes= require("./routes/campgrounds"),
         indexRoutes      = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true });
+mongoose.connect("mongodb+srv://yelpcamp:GObiga87!@yelpcamp-2dcci.mongodb.net/yelp_camp?retryWrites=true", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
