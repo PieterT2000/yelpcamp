b0e51c2f7ca91f067f2d372b5d2aad7e7cb90f3d
Pieter <pietervantuijl2000@gmail.com>
2019-02-18 09:39:20 +0000

    Include the environment variable for database

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ let     commentRoutes   = require("./routes/comments"),
         indexRoutes      = require("./routes/index");
 
 // mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true });
-mongoose.connect("mongodb+srv://yelpcamp:GObiga87!@yelpcamp-2dcci.mongodb.net/yelp_camp?retryWrites=true", { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
