d445d8bf8520243b25c3fbc8174c11c1b2424169
Pieter <pietervantuijl2000@gmail.com>
2019-02-23 14:05:29 +0000

    inital commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# yelpcamp
-YelpCamp application where anyone can view/upload/review campgrounds from all over the world.
---
+++ models/campground.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Comment  = require("./comment");
+//schema setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    price: String,
+    image: String,
+    description: String,
+    location: String,
+    lat: Number,
+    lng: Number,
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
+    },
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ]
+});
+
+
+//use schema in model
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+module.exports = Campground;
