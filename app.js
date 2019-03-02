a3698e403add87be1fc8ca202d1467af3cab6645
Pieter <pietervantuijl2000@gmail.com>
2019-03-02 18:13:13 +0000

    add new functionality

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 
-<<<<<<< HEAD
 const   express         = require("express"),
         app             = express(),
         bodyParser      = require("body-parser"),
@@ -24,42 +23,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
-=======
-const express = require('express'),
-	app = express(),
-	bodyParser = require('body-parser'),
-	mongoose = require('mongoose'),
-	passport = require('passport'),
-	LocalStrategy = require('passport-local'),
-	Campground = require('./models/campground'),
-	Comment = require('./models/comment'),
-	User = require('./models/user'),
-	methodOverride = require('method-override'),
-	flash = require('connect-flash');
-
-//requiring routes
-let commentRoutes = require('./routes/comments'),
-	campgroundRoutes = require('./routes/campgrounds'),
-	indexRoutes = require('./routes/index');
-
-const url = process.env.DATABASEURL || 'mongodb://localhost:27017/yelp_camp';
-mongoose.connect(url, { useNewUrlParser: true });
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
-app.use(methodOverride('_method'));
->>>>>>> newFeature/master
 app.use(flash());
 // seedDB(); //seed the database
 
 //passport configuration
-<<<<<<< HEAD
-app.use(require("express-session")({
-    secret: "I love Rachmaninoff's Adagio from his 2nd sonate!",
-    resave: false,
-    saveUninitialized: false
-}));
-=======
 app.use(
 	require('express-session')({
 		secret: "I love Rachmaninoff's Adagio from his 2nd sonate!",
@@ -67,7 +34,6 @@ app.use(
 		saveUninitialized: false
 	})
 );
->>>>>>> newFeature/master
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -75,23 +41,6 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-<<<<<<< HEAD
-app.use(function(req, res, next){
-    res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
-});
-
-app.use(indexRoutes);
-app.use("/campgrounds", campgroundRoutes); //add prefix to route
-app.use("/campgrounds/:id/comments", commentRoutes);
-
-
-app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("YelpCamp Server has started!");
-});
-=======
 app.use(function(req, res, next) {
 	res.locals.currentUser = req.user;
 	res.locals.error = req.flash('error');
@@ -106,4 +55,3 @@ app.use('/campgrounds/:id/comments', commentRoutes);
 app.listen((process.env.PORT, process.env.IP || 3000), function() {
 	console.log('YelpCamp Server has started!');
 });
->>>>>>> newFeature/master
